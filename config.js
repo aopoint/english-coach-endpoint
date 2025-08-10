1175a30281efe54431ab2d16f1a6290cd48d67c2
aopoint <abdelrahmanosama@gmail.com>
2025-08-10 22:52:57 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+// config.js
+window.APP_CONFIG = {
+  SUPABASE_URL: "https://cstwbjtbwwnxrsvpxqro.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNzdHdianRid3dueHJzdnB4cXJvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ4NDg1OTgsImV4cCI6MjA3MDQyNDU5OH0.D-P8NWdJoro_pzSk6qoi5_Hl6Ohz_hiTZr4-fqmyXkM",
+  API_URL: "https://english-coach-endpoint-git-main-abelrahman-osamas-projects.vercel.app/api/analyze",
+  VERSION: "v1.2.13"
+};
